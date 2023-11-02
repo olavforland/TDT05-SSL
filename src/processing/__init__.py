bde9c45ba3a62599a14dccf487a1270030c76db3
Eirik Steira <steiraeirik@gmail.com>
2023-11-02 11:49:59 +0100

    Add supervised ResNet

--- src/processing/__init__.py
+++ src/processing/__init__.py
@@ -1,11 +1,13 @@
 import os
 import torchvision.transforms as transforms
+from torchvision.datasets import SVHN
 
 from .custom_datasets import RotatedSVHN
 
-# Transformations needed to be compatible with vit_b_16 model from torchvision 
+# Transformations needed to be compatible with vit_b_16 model and ResNet from torchvision
 # Found @ https://pytorch.org/vision/main/models/generated/torchvision.models.vit_b_16.html#torchvision.models.vit_b_16
-vit_b_16_transform = transforms.Compose([
+# and @ https://pytorch.org/vision/main/models/generated/torchvision.models.resnet18.html#torchvision.models.resnet18
+transform = transforms.Compose([
     transforms.Resize(256, interpolation=transforms.InterpolationMode.BILINEAR),
     transforms.CenterCrop(224),
     transforms.ToTensor(),
@@ -13,8 +15,9 @@ vit_b_16_transform = transforms.Compose([
 ])
 
 try:
-    rotated_svhn_train = RotatedSVHN(root=os.environ['DATA_PATH'], split='train', transform=vit_b_16_transform, download=True)
-    rotated_svhn_test = RotatedSVHN(root=os.environ['DATA_PATH'], split='test', transform=vit_b_16_transform, download=True)
-
+    rotated_svhn_train = RotatedSVHN(root=os.environ['DATA_PATH'], split='train', transform=transform, download=True)
+    rotated_svhn_test = RotatedSVHN(root=os.environ['DATA_PATH'], split='test', transform=transform, download=True)
+    svhn_train = SVHN(root=os.environ['DATA_PATH'], split='test', transform=transform, download=True)
+    svhn_test = SVHN(root=os.environ['DATA_PATH'], split='test', transform=transform, download=True)
 except:
     raise Exception('DATAPATH environment variable not set. Please set it to the path where the SVHN dataset is stored.')
---
+++ src/train_resnet_supervised.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+from torch.nn import Linear
+
+# Current directory
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.environ['DATA_PATH'] = dir_path + '/data'
+
+import torch
+import pytorch_lightning as pl
+
+from torchvision.models import resnet18
+from torch.utils.data import DataLoader
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+
+from utils import get_device
+from models import LightningPredictor
+from processing import svhn_train, svhn_test
+
+
+train_loader = DataLoader(svhn_train, batch_size=64, shuffle=True, pin_memory=True)
+test_loader = DataLoader(svhn_test, batch_size=64, shuffle=False, pin_memory=True)
+
+
+model = resnet18(pretrained=True)
+model.fc = Linear(model.fc.in_features, 10)  # SVHN has 10 classes
+
+predictor = LightningPredictor(model)
+
+trainer = pl.Trainer(
+    max_epochs=10,
+    default_root_dir='./logs',
+    accelerator=get_device(as_str=True),
+    callbacks=[
+        ModelCheckpoint(save_weights_only=True, mode="max", monitor="val_acc_top5"),
+        LearningRateMonitor("epoch"),
+    ],
+)
+
+trainer.fit(predictor, train_loader)
